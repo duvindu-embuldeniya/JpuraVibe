7ff93b83461897d468dbc1cb723d191ccd01af42
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-07-30 20:20:30 +0530

    +Pswd Reset Functionalty

--- simply/simply/urls.py
+++ simply/simply/urls.py
@@ -18,11 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name = 'home/reset_password.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name = 'home/reset_password_sent.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name = 'home/reset_password_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'home/reset_password_complete.html'), name='password_reset_complete'),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
