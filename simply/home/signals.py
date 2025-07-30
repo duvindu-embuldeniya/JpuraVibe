0ade26ed3c89a1dfc872f60f3fa8d2371b9125ef
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-07-30 19:37:13 +0530

    + Pswd Reset Func

--- simply/home/signals.py
+++ simply/home/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from . models import Profile
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def create_profile(sender,instance,created,*args,**kwargs):
@@ -9,6 +12,25 @@ def create_profile(sender,instance,created,*args,**kwargs):
         Profile.objects.create(
             user = created_user
         )
+
+    subject = "Welcome"
+    message = "We are glad you are here!"
+
+    send_mail(
+        subject,
+        message,
+        settings.EMAIL_HOST_USER,
+        [instance.email],
+        fail_silently=False,
+    )
+
+
+
+@receiver(post_delete, sender = Profile)
+def delete_user(sender,instance,*args,**kwargs):
+    deleted_user = instance
+    deleted_user.user.delete()
+
 
 
 post_save.connect(create_profile, sender=User)
--- simply/simply/settings.py
+++ simply/simply/settings.py
@@ -116,6 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =os.environ.get('mail')
+EMAIL_HOST_PASSWORD =os.environ.get('app_pass')
+
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -124,9 +135,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-LOGIN_URL = 'login'
 
 
 # Default primary key field type
