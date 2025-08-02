c7b39a711030930fde9eb3522098b8d3502e7052
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-08-02 12:59:05 +0530

    fail_silently True

--- simply/home/signals.py
+++ simply/home/signals.py
@@ -21,7 +21,7 @@ def create_profile(sender,instance,created,*args,**kwargs):
             message,
             settings.EMAIL_HOST_USER,
             [instance.email],
-            fail_silently=False,
+            fail_silently=True,
         )
 
 
